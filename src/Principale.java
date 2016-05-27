82580361f9db2632f812ddb767bb8cf316a0be59
flavo <flavo@DESKTOP-I3RE3H9>
2016-05-27 08:52:41 -0400

    test

--- src/Principale.java
+++ src/Principale.java
@@ -13,15 +13,11 @@ public class Principale {
     public static void main(String[] args) {
         System.out.println("Tp1.Principale.main()");
         Integer [] monTab = {0,1,2,3,4,5,6,7,8,9};
-        String [] monTab2 = {"0","1","2","3","4","5","6","7","8","9"};
 //       Legume monLegume = new Legume(10);
         TableauPartiel tp = new TableauPartiel(monTab);
-        TableauPartiel tp1 = new TableauPartiel(monTab2);
-        TableauPartiel tp2 = new TableauPartiel(tp1);
         TableauPartiel tp99 = tp;
-//        System.out.println("Total Bien "+faireTotal(monTab));
+        System.out.println("Tableau tp "+tp.taille());
         monTab[1] = 999;
-        monTab2[3] = "Fabien";
     }
     public static int faireTotal (Integer [] Tab_par) {
         int tot = 0;
